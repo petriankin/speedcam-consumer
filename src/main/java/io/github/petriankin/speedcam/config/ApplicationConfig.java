2ba5295a42f54d53fc9f20da8b56c5789482adf5
sergei_petriankin <Sergei_petriankin@epam.com>
2021-02-16 17:32:31 +0300

    ObjectMapper bean added to the context for JsonDesirializer

---
+++ src/main/java/io/github/petriankin/speedcam/config/ApplicationConfig.java
@@ -0,0 +1,13 @@
+package io.github.petriankin.speedcam.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+}
