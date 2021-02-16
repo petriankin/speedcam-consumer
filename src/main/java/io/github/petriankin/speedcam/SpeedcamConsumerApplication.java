91bd84d944f5673e8184a7c9394a6ce5c1da689b
sergei_petriankin <Sergei_petriankin@epam.com>
2021-02-16 17:32:40 +0300

    SpeedcamConsumerApplication added

---
+++ src/main/java/io/github/petriankin/speedcam/SpeedcamConsumerApplication.java
@@ -0,0 +1,12 @@
+package io.github.petriankin.speedcam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedcamConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeedcamConsumerApplication.class, args);
+    }
+}
