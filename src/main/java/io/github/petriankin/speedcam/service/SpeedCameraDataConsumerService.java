d61e871fa636281b7ea804a0f5931a40076b69bc
sergei_petriankin <Sergei_petriankin@epam.com>
2021-02-16 17:48:27 +0300

    SpeedCameraDataConsumerService cleaned up

--- src/main/java/io/github/petriankin/speedcam/service/SpeedCameraDataConsumerService.java
+++ src/main/java/io/github/petriankin/speedcam/service/SpeedCameraDataConsumerService.java
@@ -5,7 +5,5 @@ import io.github.petriankin.speedcam.dto.CameraDataDto;
 
 public interface SpeedCameraDataConsumerService {
 
-    CameraDataDto save(CameraDataDto dto);
-
     void consume(CameraDataDto dto);
 }
--- src/main/java/io/github/petriankin/speedcam/service/impl/SpeedCameraDataConsumerServiceImpl.java
+++ src/main/java/io/github/petriankin/speedcam/service/impl/SpeedCameraDataConsumerServiceImpl.java
@@ -1,10 +1,9 @@
 package io.github.petriankin.speedcam.service.impl;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.petriankin.speedcam.dto.CameraDataDto;
+import io.github.petriankin.speedcam.service.CameraDataAnalysisService;
 import io.github.petriankin.speedcam.service.SpeedCameraDataConsumerService;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -14,23 +13,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class SpeedCameraDataConsumerServiceImpl implements SpeedCameraDataConsumerService {
 
-    private final ObjectMapper objectMapper;
-
-    @Override
-    public CameraDataDto save(CameraDataDto dto) {
-        throw new UnsupportedOperationException("Not implemented yet");
-    }
-
+    private final CameraDataAnalysisService dataAnalysisService;
 
     @Override
     @KafkaListener(topics = {"speedcam"})
     public void consume(CameraDataDto dto) {
-        log.info("consumed {}", writeValueAsString(dto));
-
-    }
-
-    @SneakyThrows
-    private String writeValueAsString(CameraDataDto dto) {
-            return objectMapper.writeValueAsString(dto);
+        log.info("consumed {}", dto);
+        dataAnalysisService.checkAndSave(dto);
     }
 }
